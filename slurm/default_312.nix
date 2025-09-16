8ae891a5a77ea09d7b216816a3974c335387e286
vonproda <david.von.proeck-zvlcil@uni-a.de>
2025-09-16 14:53:37 +0200

    ~

--- slurm/default_312.nix
+++ slurm/default_312.nix
@@ -5,17 +5,22 @@ with import (builtins.fetchGit {
   rev = "8cbadfa068534bdd8238eea362d2bf0b1d46b7e8";  # nixpkgs commit with Python 3.12.11
 }) { config = { allowUnfree = true; }; };
 
+let
+  py     = pkgs.python312;
+  pyPkgs = pkgs.python3Packages.override { python = py; };  # generic set bound to 3.12
+in
 mkShell {
   venvDir = "./_venv";
 
-  buildInputs = (with pkgs.python312Packages; [ python312 venvShellHook wheel pip ]) ++ [
-    pkgs.stdenv.cc.cc
-  ] ++ (import ./system-dependencies.nix { inherit pkgs; });
+  buildInputs =
+    [ py pyPkgs.venvShellHook pyPkgs.wheel pyPkgs.pip ]
+    ++ [ pkgs.stdenv.cc.cc ]
+    ++ (import ./system-dependencies.nix { inherit pkgs; });
 
   postShellHook = ''
     unset SOURCE_DATE_EPOCH
-    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc]}:${pkgs.lib.makeLibraryPath [pkgs.zlib]}:$LD_LIBRARY_PATH"
-    export PYTHONPATH=$venvDir/${python312.sitePackages}:$PYTHONPATH
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc ]}:${pkgs.lib.makeLibraryPath [ pkgs.zlib ]}:$LD_LIBRARY_PATH"
+    export PYTHONPATH=$venvDir/${py.sitePackages}:$PYTHONPATH
   '';
 
   postVenvCreation = ''
